e472bda69137b423871831bbd83d410ee42c3372
breezly_ <122535095+breezly101@users.noreply.github.com>
2025-06-21 19:51:25 +0100

    Update uv.config.js

--- static/uv/uv.config.js
+++ static/uv/uv.config.js
@@ -1,6 +1,6 @@
 self.__uv$config = {
     prefix: '/static/load/',
-    bare:'https://vplaza.org/bare/',
+    bare:'aHR0cHM6Ly92cGxhemEub3JnL2JhcmUv',
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
     handler: '/static/uv/uv.handler.js',
